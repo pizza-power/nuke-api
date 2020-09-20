22ee29861de3ffc92e570a73cd0cdfdf7e33d728
Matt <6135659+pizza-power@users.noreply.github.com>
2020-09-20 11:46:02 -0400

    Update main.py

--- data/main.py
+++ data/main.py
@@ -3,21 +3,16 @@
 import feedparser
 import requests
 from pymongo import MongoClient
-import json
 import datetime
 
-
-# TODO add authentication
-# TODO better logging 
-# TODO better error catching
-# TODO if data is already entered, abort entering
-# find today's date, if same as data already entered, abort
-# db.plants.find().skip(db.plants.count() - 1)
-# print(collection.find().skip(collection.count() - 1).pretty())
-# establish db connection
+TODAY = datetime.datetime.today()
+URL = "https://www.nrc.gov/public-involve/rss.cfm?feed=plant-status"
+MONGOIP = ''
+MONGOPORT = ''
+MONGOURI = 'mongodb://' + MONGOIP + ':' + MONGOPORT + '/mongo'
 
 try:
-    client = MongoClient('mongodb://*PIHERE*/*PORTHERE*/mongo')
+    client = MongoClient(MONGOURI)
     print('connected to mongodb')
 except:
     print('errro connecting')
@@ -25,13 +20,8 @@ except:
 db = client.mongo
 collection = db.plants
 
-TODAY = datetime.datetime.today()
-URL = "https://www.nrc.gov/public-involve/rss.cfm?feed=plant-status"
-
 content = requests.get(URL)
-
 rss = content.text
-
 parsed = feedparser.parse(rss)
 
 plant_list = []
@@ -45,12 +35,9 @@ for unit in plant_list:
     power_numbers_only = str.split(power_string, '%')[0] # get power as int
     post_data = {}
     post_data["date"] = str(TODAY.date())
-    # post_data["data"] = [{"name": unit[0], "power": power_numbers_only}]
     post_data['name'] = unit[0]
     post_data['power'] = power_numbers_only
-    #print(post_data)
     try:
         collection.insert_one(post_data)
-        print(post_data)
     except:
         print('Insert Error')
