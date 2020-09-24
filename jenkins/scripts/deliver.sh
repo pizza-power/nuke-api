3adfd22c71ace22588a9fbf4768935875a19b74a
Matt <6135659+pizza-power@users.noreply.github.com>
2020-09-23 20:18:42 -0400

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,5 @@
-#/usr/bin/env sh
-#/bin/bash
+#!/usr/bin/env sh
+
 
 echo 'The following "npm" command builds your Node.js/React application for'
 echo 'production in the local "build" directory (i.e. within the'
